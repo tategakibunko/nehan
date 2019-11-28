4dec0f4727cfabeeccf97d62149dd3b51e0c7fb9
tategakibunko <lambda.watanabe@gmail.com>
2019-11-28 22:29:11 +0900

    Fix indent

--- src/float-region.ts
+++ src/float-region.ts
@@ -6,12 +6,12 @@ import {
 
 export class FloatRegion {
   public maxRegion: LogicalRect;
-  public startRects: LogicalRect [];
-  public endRects: LogicalRect [];
+  public startRects: LogicalRect[];
+  public endRects: LogicalRect[];
   public cursorBefore: number; // next float chain pos.
 
-  constructor(max_size: LogicalSize, block_pos: number){
-    this.maxRegion = new LogicalRect(new LogicalCursorPos({start:0, before:0}), max_size);
+  constructor(max_size: LogicalSize, block_pos: number) {
+    this.maxRegion = new LogicalRect(new LogicalCursorPos({ start: 0, before: 0 }), max_size);
     this.cursorBefore = block_pos;
     this.startRects = [];
     this.endRects = [];
@@ -29,7 +29,7 @@ export class FloatRegion {
     return Math.max(this.maxStartRegionExtent, this.maxEndRegionExtent);
   }
 
-  public clear(){
+  public clear() {
     this.clearBoth();
     this.cursorBefore = 0;
   }
@@ -62,34 +62,34 @@ export class FloatRegion {
 
   public getSpacePos(before_pos: number): LogicalCursorPos {
     let rect = this.getStartSideRect(before_pos);
-    let start = rect? rect.end : 0;
-    return new LogicalCursorPos({start:start, before:before_pos});
+    let start = rect ? rect.end : 0;
+    return new LogicalCursorPos({ start: start, before: before_pos });
   }
 
   public pushStart(before: number, size: LogicalSize): LogicalCursorPos {
-    if(this.maxRegion.includeSize(size) === false){
+    if (this.maxRegion.includeSize(size) === false) {
       throw new Error("FloatRegion:too large size");
     }
     this.cursorBefore = Math.max(this.cursorBefore, before);
-    if(this.startRects.length === 0){
-      let pos = new LogicalCursorPos({start:0, before:this.cursorBefore});
+    if (this.startRects.length === 0) {
+      let pos = new LogicalCursorPos({ start: 0, before: this.cursorBefore });
       this.startRects.push(new LogicalRect(pos, size));
       return pos;
     }
     let top = this.startRects[this.startRects.length - 1];
     let rest_measure = this.getSpaceMeasureAt(this.cursorBefore);
     // enough measure is ready at [this.cursorBefore].
-    if(size.measure < rest_measure){
+    if (size.measure < rest_measure) {
       let rect = this.getStartSideRect(this.cursorBefore);
-      let start = rect? rect.end : 0;
+      let start = rect ? rect.end : 0;
       let before = this.cursorBefore;
-      let pos = new LogicalCursorPos({start:start, before:before});
+      let pos = new LogicalCursorPos({ start: start, before: before });
       this.startRects.push(new LogicalRect(pos, size));
       return pos;
     }
     // enough measure is not ready, so skip top block.
     this.cursorBefore += top.extent;
-    if(this.cursorBefore + size.extent >= this.maxRegion.extent){
+    if (this.cursorBefore + size.extent >= this.maxRegion.extent) {
       throw new Error("FloatRegion:no more space left.");
     }
     // recusive try.
@@ -97,29 +97,29 @@ export class FloatRegion {
   }
 
   public pushEnd(before: number, size: LogicalSize): LogicalCursorPos {
-    if(this.maxRegion.includeSize(size) === false){
+    if (this.maxRegion.includeSize(size) === false) {
       throw new Error("FloatRegion:too large size");
     }
     this.cursorBefore = Math.max(this.cursorBefore, before);
-    if(this.endRects.length === 0){
+    if (this.endRects.length === 0) {
       let pos = new LogicalCursorPos({
-	start:this.maxRegion.measure - size.measure,
-	before:this.cursorBefore
+        start: this.maxRegion.measure - size.measure,
+        before: this.cursorBefore
       });
       this.endRects.push(new LogicalRect(pos, size));
       return pos;
     }
     let top = this.endRects[this.endRects.length - 1];
     let rest_measure = this.getSpaceMeasureAt(this.cursorBefore);
-    if(size.measure < rest_measure){
+    if (size.measure < rest_measure) {
       let rect = this.getEndSideRect(this.cursorBefore);
-      let start = rect? rect.start - size.measure : this.maxRegion.measure - size.measure;
-      let pos = new LogicalCursorPos({start:start, before:this.cursorBefore});
+      let start = rect ? rect.start - size.measure : this.maxRegion.measure - size.measure;
+      let pos = new LogicalCursorPos({ start: start, before: this.cursorBefore });
       this.endRects.push(new LogicalRect(pos, size));
       return pos;
     }
     this.cursorBefore += top.extent;
-    if(this.cursorBefore + size.extent >= this.maxRegion.extent){
+    if (this.cursorBefore + size.extent >= this.maxRegion.extent) {
       throw new Error("FloatRegion:no more space left.");
     }
     return this.pushEnd(this.cursorBefore, size);
@@ -145,10 +145,10 @@ export class FloatRegion {
   private getEndSideRect(before_pos: number): LogicalRect | null {
     return this.getSideRect(this.endRects, before_pos);
   }
-  
+
   private getStartSideRectMeasure(before_pos: number): number {
     let rect = this.getStartSideRect(before_pos);
-    if(!rect){
+    if (!rect) {
       return 0;
     }
     return rect.end;
@@ -156,13 +156,13 @@ export class FloatRegion {
 
   private getEndSideRectMeasure(before_pos: number): number {
     let rect = this.getEndSideRect(before_pos);
-    if(!rect){
+    if (!rect) {
       return 0;
     }
     return this.maxRegion.measure - rect.start;
   }
 
-  private getMaxSideCursorBeforeFrom(rects: LogicalRect []): number {
+  private getMaxSideCursorBeforeFrom(rects: LogicalRect[]): number {
     return rects.reduce((max, rect) => Math.max(max, rect.after), this.cursorBefore);
   }
 
@@ -172,11 +172,11 @@ export class FloatRegion {
   }
   */
 
-  private getSideRect(floats: LogicalRect [], before_pos: number): LogicalRect | null {
-    for(let i = floats.length - 1; i >= 0; i--){
+  private getSideRect(floats: LogicalRect[], before_pos: number): LogicalRect | null {
+    for (let i = floats.length - 1; i >= 0; i--) {
       let rect = floats[i];
-      if(rect.before <= before_pos && before_pos < rect.after){
-	return rect;
+      if (rect.before <= before_pos && before_pos < rect.after) {
+        return rect;
       }
     }
     return null;
