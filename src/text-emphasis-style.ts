b0783f9ea44cd1461e323b32285c1b76948bad80
tategakibunko <lambda.watanabe@gmail.com>
2019-12-05 23:42:29 +0900

    Fix indent

--- src/text-emphasis-style.ts
+++ src/text-emphasis-style.ts
@@ -25,17 +25,17 @@ export interface TextEmphasisStyleValue {
   mark: TextEmphasisMark
 }
 
-let empha_marks: {[keyword: string]: string} = {
-  "filled dot":"\u2022",
-  "open dot":"\u25E6",
-  "filled circle":"\u25CF",
-  "open circle":"\u25CB",
-  "filled double-circle":"\u25C9",
-  "open double-circle":"\u25CE",
-  "filled triangle":"\u25B2",
-  "open triangle":"\u25B3",
-  "filled sesame":"\uFE45",
-  "open sesame":"\uFE46"
+let empha_marks: { [keyword: string]: string } = {
+  "filled dot": "\u2022",
+  "open dot": "\u25E6",
+  "filled circle": "\u25CF",
+  "open circle": "\u25CB",
+  "filled double-circle": "\u25C9",
+  "open double-circle": "\u25CE",
+  "filled triangle": "\u25B2",
+  "open triangle": "\u25B3",
+  "filled sesame": "\uFE45",
+  "open sesame": "\uFE46"
 }
 
 export class TextEmphasisStyle {
@@ -43,8 +43,8 @@ export class TextEmphasisStyle {
   public mark: TextEmphasisMark;
 
   static property: string = "text-emphasis-style";
-  static strokes: string [] = Utils.Enum.toValueArray(TextEmphasisStroke);
-  static marks: string [] = Utils.Enum.toValueArray(TextEmphasisMark);
+  static strokes: string[] = Utils.Enum.toValueArray(TextEmphasisStroke);
+  static marks: string[] = Utils.Enum.toValueArray(TextEmphasisMark);
 
   static isStrokeValue(value: string): boolean {
     return TextEmphasisStyle.strokes.indexOf(value) >= 0;
@@ -57,37 +57,37 @@ export class TextEmphasisStyle {
   static parse(css_text: CssText): TextEmphasisStyleValue {
     let stroke = "", mark = "";
     css_text.split().forEach(value => {
-      if(TextEmphasisStyle.isStrokeValue(value)){
-	stroke = value;
-      } else if(TextEmphasisStyle.isMarkValue(value)){
-	mark = value;
+      if (TextEmphasisStyle.isStrokeValue(value)) {
+        stroke = value;
+      } else if (TextEmphasisStyle.isMarkValue(value)) {
+        mark = value;
       }
     });
     return {
-      stroke:stroke as TextEmphasisStroke,
-      mark:mark as TextEmphasisMark
+      stroke: stroke as TextEmphasisStroke,
+      mark: mark as TextEmphasisMark
     };
   }
 
   static load(element: HtmlElement): TextEmphasisStyle {
     let prop = "text-emphasis-style";
     let value = CssCascade.getValue(element, prop);
-    let css_text = new CssText({prop:prop, value:value});
+    let css_text = new CssText({ prop: prop, value: value });
     let es_value = TextEmphasisStyle.parse(css_text);
     return new TextEmphasisStyle(es_value);
   }
 
-  constructor(value: TextEmphasisStyleValue){
+  constructor(value: TextEmphasisStyleValue) {
     this.stroke = DefaultCss.selectOrDefault(
       "text-emphasis-style-stroke", value.stroke, TextEmphasisStyle.strokes
     ) as TextEmphasisStroke;
 
-    this.mark = (this.stroke === "none")? TextEmphasisMark.DOT : DefaultCss.selectOrDefault(
+    this.mark = (this.stroke === "none") ? TextEmphasisMark.DOT : DefaultCss.selectOrDefault(
       "text-emphasis-style-mark", value.mark, TextEmphasisStyle.marks
     ) as TextEmphasisMark;
   }
 
-  public get values(): string [] {
+  public get values(): string[] {
     return [this.stroke, this.mark];
   }
 
