16360fda01c72fbe5ef19cfe30c4f7c477a9dc75
tategakibunko <lambda.watanabe@gmail.com>
2020-02-27 11:52:51 +0900

    separated source(IReResizer)

--- src/re-node-generator.ts
+++ src/re-node-generator.ts
@@ -6,20 +6,9 @@ import {
   LogicalSize,
   PhysicalSize,
   ReReducer,
+  IReResizer,
+  ReNormalResizer,
 } from './public-api';
-
-export interface IReResizer {
-  resize: (context: ReFormatContext, originalSize: LogicalSize, maxSize: LogicalSize) => LogicalSize;
-}
-
-export class ReNormalResizer implements IReResizer {
-  static instance = new ReNormalResizer();
-  protected constructor() { }
-
-  resize(context: ReFormatContext, originalSize: LogicalSize, maxSize: LogicalSize): LogicalSize {
-    return originalSize.resize(maxSize);
-  }
-}
 
 export class ReNodeGenerator implements ILogicalNodeGenerator {
   private generator: Generator<LayoutResult>;
