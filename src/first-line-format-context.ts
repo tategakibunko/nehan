495a2b0eb93f7f566953d442469c1edbb93462d5
tategakibunko <lambda.watanabe@gmail.com>
2020-03-04 19:30:23 +0900

    Added comment

--- src/first-line-format-context.ts
+++ src/first-line-format-context.ts
@@ -8,20 +8,10 @@ export class FirstLineFormatContext extends FlowFormatContext {
   public addLine(block: LogicalLineNode) {
     const isFirstLine = !this.isLineCreated();
     super.addLine(block);
+    // In '::first-line' element, original env is only used for first-line block,
+    // so after adding first line block, we should switch env to parent one.
     if (isFirstLine && this.parent) {
       this.env = this.parent.env;
-      let child = this.child;
-      while (child) {
-        if (child instanceof TextNodeGenerator) {
-          break;
-        }
-        child.context.env = this.parent.env;
-        if (child instanceof FlowFormatContext) {
-          child = child.child;
-        } else {
-          break;
-        }
-      }
     }
   }
 }
