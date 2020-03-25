52efd6cbb136d67eb87688aa2ea7da8d4044e138
tategakibunko <lambda.watanabe@gmail.com>
2020-03-25 18:55:32 +0900

    while -> if

--- src/re-node-generator.ts
+++ src/re-node-generator.ts
@@ -42,14 +42,17 @@ export class ReNodeGenerator implements ILogicalNodeGenerator {
       logicalSize = this.resizer.resize(this.context, logicalSize, maxSize);
       physicalSize = logicalSize.getPhysicalSize(writingMode);
     }
-    while (this.context.restExtent < logicalSize.extent) {
+    // while (this.context.restExtent < logicalSize.extent) {
+    if (this.context.restExtent < logicalSize.extent) {
       yield LayoutResult.pageBreak(this.context, "re extent is not enough for restExtent");
     }
-    while (this.context.env.display.isInlineLevel() && this.context.restMeasure < logicalSize.measure) {
+    // while (this.context.env.display.isInlineLevel() && this.context.restMeasure < logicalSize.measure) {
+    if (this.context.env.display.isInlineLevel() && this.context.restMeasure < logicalSize.measure) {
       yield LayoutResult.lineBreak(this.context, "restMeasure is not enough for inline re");
     }
     // Check restExtent again after lineBreak.
-    while (this.context.restExtent < logicalSize.extent) {
+    // while (this.context.restExtent < logicalSize.extent) {
+    if (this.context.restExtent < logicalSize.extent) {
       yield LayoutResult.pageBreak(this.context, "re extent is not enough for restExtent");
     }
     yield this.context.acceptLayoutReducer(this.reducer, logicalSize, physicalSize);
