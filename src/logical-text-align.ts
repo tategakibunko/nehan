076804873c37d7073fa1c17e6a8b9ad54370da85
tategakibunko <lambda.watanabe@gmail.com>
2020-03-28 22:42:18 +0900

    Use direct value type

--- src/logical-text-align.ts
+++ src/logical-text-align.ts
@@ -1,26 +1,15 @@
 import {
-  Utils,
-  BasicStyle,
   HtmlElement,
   CssCascade,
-  NativeStyleMap,
 } from "./public-api";
 
-export enum LogicalTextAlignValue {
-  START = "start",
-  END = "end",
-  CENTER = "center",
-  JUSTIFY = "justify"
-}
+export type LogicalTextAlignValue = "start" | "end" | "center" | "justify";
 
 export class LogicalTextAlign {
-  public value: string;
-  static values: string[] = Utils.Enum.toValueArray(LogicalTextAlignValue);
+  public value: LogicalTextAlignValue;
 
   constructor(value: LogicalTextAlignValue) {
-    this.value = BasicStyle.selectOrDefault(
-      "text-align", value, LogicalTextAlign.values
-    );
+    this.value = value;
   }
 
   static load(element: HtmlElement): LogicalTextAlign {
@@ -29,49 +18,18 @@ export class LogicalTextAlign {
   }
 
   public isStart(): boolean {
-    return this.value === LogicalTextAlignValue.START;
+    return this.value === "start";
   }
 
   public isEnd(): boolean {
-    return this.value === LogicalTextAlignValue.END;
+    return this.value === "end";
   }
 
   public isCenter(): boolean {
-    return this.value === LogicalTextAlignValue.CENTER;
+    return this.value === "center";
   }
 
   public isJustify(): boolean {
-    return this.value === LogicalTextAlignValue.JUSTIFY;
+    return this.value === "justify";
   }
-
-  /*
-  public getCss(box: LogicalBox): NativeStyleMap {
-    if (box.isTextVertical()) {
-      return this.getCssVert(box);
-    }
-    return this.getCssHori(box);
-  }
-
-  public getCssVert(box: LogicalBox): NativeStyleMap {
-    let css = new NativeStyleMap();
-    let gap = box.size.measure - box.autoSize.measure;
-    if (this.isEnd()) {
-      css.set("margin-top", gap + "px");
-    } else if (this.isCenter()) {
-      css.set("margin-top", Math.floor(gap / 2) + "px");
-    }
-    return css;
-  }
-
-  public getCssHori(box: LogicalBox): NativeStyleMap {
-    let css = new NativeStyleMap();
-    let gap = box.size.measure - box.autoSize.measure;
-    if (this.isEnd()) {
-      css.set("margin-left", gap + "px");
-    } else if (this.isCenter()) {
-      css.set("margin-left", Math.floor(gap / 2) + "px");
-    }
-    return css;
-  }
-  */
 }
