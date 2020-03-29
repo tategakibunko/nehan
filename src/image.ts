ef345af77e917d52ffe14edfb42e8496e00d734a
tategakibunko <lambda.watanabe@gmail.com>
2020-03-29 21:45:30 +0900

    Revert

--- src/image.ts
+++ src/image.ts
@@ -1,30 +1,19 @@
 import {
-  Utils,
   HtmlElement,
   ResourceLoaderContext,
   Config,
 } from "./public-api";
 
 export class Image {
-  static exists(element: HtmlElement): boolean {
-    const width = Utils.atoi(element.getAttribute("width") || "0");
-    const height = Utils.atoi(element.getAttribute("height") || "0");
-    return width > 0 && height > 0;
-  }
-
   static load(element: HtmlElement, context?: ResourceLoaderContext): Promise<HtmlElement> {
     return new Promise<HtmlElement>((resolve, reject) => {
       const $node = element.$node as HTMLImageElement;
       const image = document.createElement("img") as HTMLImageElement;
       image.src = $node.src;
       image.onload = (evt: Event) => {
-        //console.log("image.onload:width=%d, height=%d", image.width, image.height);
-        if (image.width > 0) {
-          $node.setAttribute("width", String(image.width));
-        }
-        if (image.height > 0) {
-          $node.setAttribute("height", String(image.height));
-        }
+        // console.log("image.onload:width=%d, height=%d", image.width, image.height);
+        $node.setAttribute("width", String(image.width));
+        $node.setAttribute("height", String(image.height));
         if (context) {
           if (Config.debugResourceLoader) {
             console.info("%s is successfully loaded(%dx%d)", image.src, image.width, image.height);
