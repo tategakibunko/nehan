2e33ea992cb47c5972216651c1c9149a93f78278
tategakibunko <lambda.watanabe@gmail.com>
2020-04-10 15:23:23 +0900

    Removed unused filter

--- src/child-node-filter.ts
+++ src/child-node-filter.ts
@@ -9,47 +9,6 @@ export interface ChildNodeFilter {
   visit: (element: HtmlElement) => boolean;
 }
 
-/*
-  # remove white space element between blocks
-
-  <div>[white-spaces
-     ]<p>some text</p>[white-spaces
-  ]</div>
-
-  =>
-  
-  <div><p>some text</p></div>
-*/
-export class WhiteSpaceEliminator implements ChildNodeFilter {
-  static instance = new WhiteSpaceEliminator();
-  private constructor() { }
-
-  private isBlockElement(element: HtmlElement): boolean {
-    const display = Display.load(element);
-    return display.isBlockLevel();
-  }
-
-  visit(element: HtmlElement): boolean {
-    if (!WhiteSpace.isWhiteSpaceElement(element)) {
-      return true;
-    }
-    const parent = element.parent;
-    // remove first white-spaces in block
-    if (parent && element.isFirstChild() && this.isBlockElement(parent)) {
-      return false;
-    }
-    const prev = element.previousSibling;
-    if (!prev) {
-      return true;
-    }
-    // remove whitespaces after block, or before block.
-    if (this.isBlockElement(prev)) {
-      return false;
-    }
-    return true;
-  }
-}
-
 export class ValidBlockSelector implements ChildNodeFilter {
   static instance = new ValidBlockSelector();
   private constructor() { }
