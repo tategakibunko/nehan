958f433572830f8385599df4b2620123efe1218a
tategakibunko <lambda.watanabe@gmail.com>
2020-04-13 07:24:28 +0900

    Use data-src while loading image

--- src/image.ts
+++ src/image.ts
@@ -8,11 +8,26 @@ export class Image {
   // 1. load image data from [srcAttr].
   // 2. get image width/height.
   // 3. set width/height to attribute.
-  static load(element: HtmlElement, srcAttr: string, context?: ResourceLoaderContext): Promise<HtmlElement> {
+  static load(element: HtmlElement, srcAttr: string, context: ResourceLoaderContext): Promise<HtmlElement> {
     return new Promise<HtmlElement>((resolve, reject) => {
       const $node = element.$node as HTMLElement;
       const image = document.createElement("img") as HTMLImageElement;
-      image.setAttribute("src", $node.getAttribute(srcAttr) || "");
+      const src = $node.getAttribute(srcAttr);
+      if (src) {
+        image.setAttribute("src", src);
+      }
+      const dataSrc = $node.getAttribute("data-src");
+      if (dataSrc) {
+        image.setAttribute("data-src", dataSrc);
+      }
+      if (!src && !dataSrc) {
+        if (Config.debugResourceLoader) {
+          console.error("Invalid resource. Both src and data-src are not defined:", element);
+        }
+        context.fail(element);
+        resolve(element);
+        return;
+      }
       image.onload = (evt: Event) => {
         // console.log("image.onload:width=%d, height=%d", image.width, image.height);
         $node.setAttribute("width", String(image.width));
