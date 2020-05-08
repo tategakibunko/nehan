ec4d9b21adf4622939069a9babb3ad7c69d9a35b
tategakibunko <lambda.watanabe@gmail.com>
2020-05-08 15:08:26 +0900

    Added comment

--- src/color.ts
+++ src/color.ts
@@ -4,9 +4,32 @@ import {
   ILogicalCssEvaluator,
 } from './public-api';
 
+/*
+  To treat following case correctly, we must control 'color' property in nehan layer.
+
+  <div class="parent">
+    <b style="color:red">red text1
+      <p>this is also red</p>
+      red text2
+    </b>
+  </div>
+
+  In nehan, block element(<p>) inside inline(<b>) is propagated to parent block(div.parent) like this.
+
+  <div class="parent">
+    <b style="color:red">red text1</b>
+    <p>this is also red</p>
+    <b style="color:red">red text2</b>
+  </div>
+
+  Then DOM of <p> is placed under div.parent(not <b>!).
+  As a result, <b>.color is not inherited to <p> in output form of DOM collectly.
+  Because parent of <p> is no longer <b>(but div.parent) in output form of DOM!
+
+  This is why we have to treat 'color' property inside nehan.
+*/
 export class Color {
-  constructor(public value: string) {
-  }
+  private constructor(public value: string) { }
 
   static load(element: HtmlElement): Color {
     const value = element.computedStyle.getPropertyValue("color") || "inherit";
